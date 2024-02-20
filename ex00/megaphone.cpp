cf0cdfd734c67842ca3afa3d4c1ec18a32a8ecf9
jariza-o <juanarizaordonez@gmail.com>
2024-02-20 21:28:54 +0100

    Ex00 started

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jariza-o <jariza-o@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 21:23:14 by jariza-o          #+#    #+#             */
+/*   Updated: 2024/02/20 21:27:44 by jariza-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv) {
+	
+	(void)argv;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDbACK NOISSE *" << std::endl;
+	return 0;
+}
