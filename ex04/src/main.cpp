02f79f6ad87a0c42eb06e95fef0629c697cd4fae
jariza-o <juanarizaordonez@gmail.com>
2024-02-29 23:05:26 +0100

    ex04 started

---
+++ ex04/src/main.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jariza-o <jariza-o@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/29 23:02:03 by jariza-o          #+#    #+#             */
+/*   Updated: 2024/02/29 23:04:30 by jariza-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv) {
+	if (argc != 3) {
+		std::cout << "The number of parameters is incorrect." << std::endl;
+		return 1;
+	}
+	
+}
