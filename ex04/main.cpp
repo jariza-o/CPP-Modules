e87a831fe5e56592e74ca2160379fdd4e081c672
jariza-o <juanarizaordonez@gmail.com>
2024-03-01 16:23:41 +0100

    ex04 in process

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,15 +6,25 @@
 /*   By: jariza-o <jariza-o@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 23:02:03 by jariza-o          #+#    #+#             */
-/*   Updated: 2024/02/29 23:04:30 by jariza-o         ###   ########.fr       */
+/*   Updated: 2024/03/01 16:23:13 by jariza-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 int	main(int argc, char **argv) {
-	if (argc != 3) {
-		std::cout << "The number of parameters is incorrect." << std::endl;
+	if (argc != 4) {
+		std::cout << "Error: The number of parameters is incorrect." << std::endl;
+		std::cout << "Correct Input: ./Replace <filename> <s1> <s2>" << std::endl;
+		return 1;
+	}
+	
+	std::string	file = argv[1];
+	std::string	s1 = argv[2];
+	std::string	s2 = argv[3];
+	
+	if (file.empty() || s1.empty()){ // s2 creo que puede ser empty para sustituir algo por nada
+		std::cout << "Error: fileSTR or s1 is empty." << std::endl;
 		return 1;
 	}
 	
