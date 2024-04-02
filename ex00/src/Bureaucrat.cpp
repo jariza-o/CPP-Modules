d53f131f193c720df80f953e0bcd4f7e6330dc0b
JuanAriza-Premo <juan.ariza@grupopremo.com>
2024-04-02 08:56:34 +0200

    ex00 in progress

--- ex00/src/Bureaucrat.cpp
+++ ex00/src/Bureaucrat.cpp
@@ -21,7 +21,7 @@ Bureaucrat::Bureaucrat( std::string name ) : _name(name), _grade(1) {
 }
 Bureaucrat::Bureaucrat( int grade ) : _name("void") {
 	std::cout << "Bureaucrat Grade Constructor called." << std::endl;
-	if (grade < 1 || grade > 150) {
+	if (grade < 1 || grade > 150) { // AQUI TENGO QUE USAR EXCEPCIONES TRY CATH
 		this->_grade = 1;
 		std::cout << "The Grade Int is incorrect." << std::endl;
 	}
@@ -30,10 +30,37 @@ Bureaucrat::Bureaucrat( int grade ) : _name("void") {
 }
 Bureaucrat::Bureaucrat( std::string name, int grade ) : _name(name) {
 	std::cout << "Bureaucrat Name and Grade Constructor called." << std::endl;
-	if (grade < 1 || grade > 150) {
+	if (grade < 1 || grade > 150) { // AQUI TENGO QUE USAR EXCEPCIONES TRY CATH
 		this->_grade = 1;
 		std::cout << "The Grade Int is incorrect." << std::endl;
 	}
 	else
 		this->_grade = grade;
+}
+
+Bureaucrat::~Bureaucrat() {
+	std::cout << "Bureaucrat Destructor called." << std::endl;
+}
+
+std::string	Bureaucrat::getName() const {
+	return this->_name;
+}
+
+int	Bureaucrat::getGrade() const {
+	return this->_grade;
+}
+
+// COMO 1 es es mas alto y 150 el mas bajo la incrementación y decrementación está mal
+
+void	Bureaucrat::incrementGrade( int i ) {
+	if ((this->_grade + i) > 0 && (this->_grade + i) < 151)  // AQUI TENGO QUE USAR EXCEPCIONES TRY CATH
+		this->_grade += i;
+	else
+		std::cout << "The Grade Sum is incorrect." << std::endl;
+}
+void	Bureaucrat::decrementGrade( int i ) {
+	if ((this->_grade - i) > 0 && (this->_grade - i) < 151) // AQUI TENGO QUE USAR EXCEPCIONES TRY CATH
+		this->_grade -= i;
+	else
+		std::cout << "The Grade Subtract is incorrect." << std::endl;
 }
