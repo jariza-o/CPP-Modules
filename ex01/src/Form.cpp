6077ae5f48c86428c3ee2b413fd678f1fcfe8c0e
jariza-o <juanarizaordonez@gmail.com>
2024-04-08 15:38:22 +0200

    ex01 in progress

--- ex01/src/Form.cpp
+++ ex01/src/Form.cpp
@@ -6,7 +6,28 @@
 /*   By: jariza-o <jariza-o@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 17:46:30 by jariza-o          #+#    #+#             */
-/*   Updated: 2024/04/02 17:46:31 by jariza-o         ###   ########.fr       */
+/*   Updated: 2024/04/08 15:37:32 by jariza-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/Form.hpp"
+
+Form() : _sign(){
+	std::cout << "Form Void Constructor called." << std::endl;
+}
+
+Form(std::string name) {
+	
+}
+
+Form(int signGrade, int execGrade) {
+    
+}
+
+Form(std::string name, int signGrade) {
+    
+}
+
+Form(std::string name, int signGrade, int execGrade) {
+    
+}
