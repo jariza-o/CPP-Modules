e5be831153bbdfe85484d7a5eb916327c6c46462
JuanAriza-Premo <juan.ariza@grupopremo.com>
2024-04-17 12:53:03 +0200

    ex00 converter working, limits failling

--- ex00/src/ScalarConverter.cpp
+++ ex00/src/ScalarConverter.cpp
@@ -12,6 +12,9 @@
 
 #include "../includes/ScalarConverter.hpp"
 #include <cmath>
+#include <stdio.h>
+#include <cstdlib>
+#include <cfloat>
 
 ScalarConverter::ScalarConverter() {
 	// std::cout << "ScalaConverter Void Constructor called." << std::endl;
@@ -40,26 +43,73 @@ void	ScalarConverter::convert(std::string src) {
 	double		vDouble = 0.0;
 
 	this->_type = this->checkArgument(src);
+	std::cout << "TYPE: " << this->_type << std::endl;
 	if (this->_type == 4) {
 		vChar = "impossible";
-		vInt = "impossible"; // Como paso el str impossible a INT???? debe de ser con reinterpretet??
-		vFloat = std::sqrt(-1); // COMO HAGO PARA QUE IMPRIMA LA f????
+		vFloat = std::sqrt(-1);
 		vDouble = std::sqrt(-1);
+		std::cout << "char: " << vChar << std::endl;
+		std::cout << "int: " << "impossible" << std::endl;
+		std::cout << "float :" << vFloat<< "f" << std::endl;
+		std::cout << "double: " << vDouble << std::endl;
+		return ;
 	}
 	else if (this->_type == 5) {
 		vChar = "impossible";
-		vInt = "impossible"; // Como paso el str impossible a INT????
-		vFloat = 1.0 / 0.0; // COMO HAGO PARA QUE IMPRIMA LA f????
-		vDouble = 1.0 / 0.0;	
+		vFloat = 1.0 / 0.0;
+		vDouble = 1.0 / 0.0;
+		std::cout << "char: " << vChar << std::endl;
+		std::cout << "int: " << "impossible" << std::endl;
+		std::cout << "float: " << vFloat<< "f" << std::endl;
+		std::cout << "double: " << vDouble << std::endl;
+		return ;
 	}
-	std::cout << "char:" << vChar << std::endl;
-	std::cout << "int:" << vInt << std::endl;
-	std::cout << "float:" << vFloat<< std::endl;
-	std::cout << "double:" << vDouble << std::endl;
+	else if (this->_type == 0) {
+		vChar = "impossible";
+		vInt = 2;
+		vFloat = std::sqrt(-1);
+		vDouble = std::sqrt(-1);
+	}
+	else if (this->_type == 1) { // FALTA POR COMPROBAR MAX Y MIN
+		vChar = "Non displayable";
+		vInt = atoi(src.c_str());
+		vFloat = static_cast<float>(vInt);
+		vDouble = static_cast<double>(vInt);
+		std::cout << "char: " << vChar << std::endl;
+		std::cout << "int: " << vInt << std::endl;
+		std::cout << "float: " << vFloat<< ".0f" << std::endl;
+		std::cout << "double: " << vDouble << std::endl;
+	}
+	else if (this->_type == 2 || this->_type == 3) { // FALTA POR COMPROBAR MAX Y MIN
+		vChar = "'*'";
+
+		char* endPtr;
+		vDouble = std::strtod(src.c_str(), &endPtr); // Lo de endPTR es para la comprobación de abajo, pero si la dejo no va
+		// if (*endPtr != '\0') {
+		// 	std::cout << "No se pudo convertir completamente el string a número" << std::endl;
+		// 	return ;
+		// }
+		if (vFloat < -DBL_MAX || vFloat > DBL_MAX) {
+			 throw std::out_of_range("The namber isnt in the correct range");
+		}
+		vInt = static_cast<int>(vDouble);
+		vFloat = static_cast<float>(vDouble);
+		if (this->_type == 2) {
+			if (vFloat < -FLT_MAX || vFloat > FLT_MAX) {
+				throw std::out_of_range("The namber isnt in the correct range");
+			}
+		}
+		std::cout << "char: " << vChar << std::endl;
+		std::cout << "int: " << vInt << std::endl;
+		std::cout << "float: " << vFloat<< "f" << std::endl;
+		std::cout << "double: " << vDouble << std::endl;
+	}
+
 }
 
 int	ScalarConverter::checkArgument(std::string src) {
 	int	type = 0;
+	int	aux = 0;
 
 	if (src == "nan")
 		return 4;
@@ -68,12 +118,16 @@ int	ScalarConverter::checkArgument(std::string src) {
 	for (int i = 0; src[i]; i++) {
 		if (src[i] >= 48 && src[i] <= 57)
 			type = 1;
-		else if (src[i] == '.' && type == 1)
+		else if (src[i] == '.' && type == 1){
 			type = 3;
-		else if (src[i] == 'f' && !src[i + 1] && type == 3) // un float entero creo que tiene que ser 42.0f, si no necesita el .0 añadir que si es int tamb entre
+			aux = 1;
+		}
+		else if (src[i] == 'f' && src[i + 1] == '\0' && aux == 1) // un float entero creo que tiene que ser 42.0f, si no necesita el .0 añadir que si es int tamb entre
 			return 2;
 		else 
 			return 0;
 	}
+	if (type == 1 && aux == 1)
+		return 3;
 	return type;
 }
