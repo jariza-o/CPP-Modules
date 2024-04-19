c1a74949c95d2a2b80ba3a9416f685d7ab824a3f
jariza-o <jariza-o@student.42malaga.com>
2024-04-19 16:58:14 +0200

    ex01 finished

--- ex01/includes/iter.hpp
+++ ex01/includes/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: jariza-o <jariza-o@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 16:16:00 by jariza-o          #+#    #+#             */
-/*   Updated: 2024/04/19 16:49:14 by jariza-o         ###   ########.fr       */
+/*   Updated: 2024/04/19 16:56:48 by jariza-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 #include <iostream>
 
-template <typename T>
-void    iter(T &array, int length, T  (*func)(T)) {
+template <typename T, typename F>
+void    iter(T* array, int length, F  func) {
 	for (int i = 0; i < length; i++) {
 		func(array[i]);
 	}
