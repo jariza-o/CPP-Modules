53e635c2792b665467c8f52052284d25349770bb
jariza-o <juanarizaordonez@gmail.com>
2024-04-20 15:41:54 +0200

    ex01 comment added

--- ex01/includes/iter.hpp
+++ ex01/includes/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: jariza-o <jariza-o@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 16:16:00 by jariza-o          #+#    #+#             */
-/*   Updated: 2024/04/20 13:05:42 by jariza-o         ###   ########.fr       */
+/*   Updated: 2024/04/20 15:41:24 by jariza-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 #include <iostream>
 
-template <typename T, typename F>
+template <typename T, typename F> // CUando son 2 tipos distintos hay que crear 2 tipos de template
 void    iter(T* array, int length, F  func) {
 	for (int i = 0; i < length; i++) {
 		func(array[i]);
