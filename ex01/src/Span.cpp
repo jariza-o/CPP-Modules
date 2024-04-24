7eb79ec4b4a1614186b2fd6f040688839bc52423
jariza-o <jariza-o@student.42malaga.com>
2024-04-24 18:33:46 +0200

    ex00 started

---
+++ ex01/src/Span.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jariza-o <jariza-o@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 17:22:35 by jariza-o          #+#    #+#             */
+/*   Updated: 2024/04/24 17:24:06 by jariza-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Span.hpp"
+
+Span::Span() : _N(0) {
+	
+}
+
+Span::Span(const unsigned int N) {
+	
+}
+
+Span::Span(const Span& src) {
+	
+}
+
+Span::~Span() {
+	
+}
+
+
+Span&	Span::operator=(const Span& src) {
+	
+}
+
+
+void	Span::addNumber(int singleNumber) {
+	
+}
+
+
+void	Span::shortestSpan() {
+	
+}
+
+void	Span::longestSpan() {
+	
+}
