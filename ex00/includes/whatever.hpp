bbc3e6f4bc41a3b22dc71d1a45e42167391cd759
jariza-o <jariza-o@student.42malaga.com>
2024-04-24 14:57:56 +0200

    ex00 error solved and eval files added

--- ex00/includes/whatever.hpp
+++ ex00/includes/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: jariza-o <jariza-o@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 13:44:55 by jariza-o          #+#    #+#             */
-/*   Updated: 2024/04/19 16:13:01 by jariza-o         ###   ########.fr       */
+/*   Updated: 2024/04/24 14:56:24 by jariza-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ T   min( T const& x, T const& y) {
 		return ((x < y) ? x : y);
 }
 template <typename T> 
-void	swap( T x, T y) {
+void	swap( T &x, T &y) {
 	T	x_sw = y;
 	
 	y = x;
